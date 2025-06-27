96e6f0ae6ba4c02c9f7bf2d6f88cf1f4585a0f51
dhofsetz <dbrighthd@gmail.com>
2025-06-26 18:00:46 -0700

    removed  zonai wings, removed debug line in program

--- autoElytraETF/Program.cs
+++ autoElytraETF/Program.cs
@@ -111,10 +111,6 @@ class Program
     }
     static void copyAnimaticaPropertiesFile(string animaticaPropertiesPath)
     {
-        foreach (string key in pathToNum.Keys)
-        {
-            Console.WriteLine(key);
-        }
         var sb = new StringBuilder();
 
         foreach (string line in File.ReadLines(animaticaPropertiesPath))
